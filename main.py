fa76f90a81f2ee02c6490319b8106bf2bcb7c12f
Jason Shockness <51177417+jshockness@users.noreply.github.com>
2022-03-08 09:23:22 +0000

    second solution

--- main.py
+++ main.py
@@ -3,3 +3,10 @@ numbers_added = 0
 for number in range(2,101,2):
     numbers_added += number
 print(numbers_added)
+
+# Different way of solving problem
+total = 0
+for number in range(1, 101):
+  if number % 2 == 0:
+    total += number
+print(total)
